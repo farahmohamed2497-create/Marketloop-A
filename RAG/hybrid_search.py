c8a3bbef0635088674e7615f315e484281ff763c
farahmohamed2497-create <farahmohamed2497@gmail.com>
2026-08-07 01:45:01 +0300

    feat: implement hybrid search

---
+++ RAG/hybrid_search.py
@@ -0,0 +1,54 @@
+class HybridSearch:
+
+    def __init__(self, vector_store, keyword_store):
+        self.vector_store = vector_store
+        self.keyword_store = keyword_store
+
+    def search(
+        self,
+        query_embedding,
+        query_text,
+        top_k=5,
+        alpha=0.7
+    ):
+
+        vector_results = self.vector_store.search(
+            query_embedding,
+            top_k=top_k * 2
+        )
+
+        keyword_results = self.keyword_store.query(
+            query_text,
+            top_k=top_k * 2
+        )
+
+        scores = {}
+
+        for r in vector_results:
+            text = r["text"]
+
+            scores[text] = {
+                "text": text,
+                "metadata": r["metadata"],
+                "score": alpha * r["score"]
+            }
+
+        for r in keyword_results:
+            text = r["payload"]
+
+            if text not in scores:
+                scores[text] = {
+                    "text": text,
+                    "metadata": r["metadata"],
+                    "score": 0
+                }
+
+            scores[text]["score"] += (1 - alpha) * r["score"]
+
+        ranked = sorted(
+            scores.values(),
+            key=lambda x: x["score"],
+            reverse=True
+        )
+
+        return ranked[:top_k]
